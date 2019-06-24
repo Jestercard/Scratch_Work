ed72faffc12dbbf0a0505f6f4e73192d498ebc23
Jesse Barajas <jb19932010@yahoo.com>
2019-06-24 12:46:31 -0500

    Formatting of game field

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -48,15 +48,15 @@ namespace TicTacToe
 
         public static void CreateTicTacToe()
         {
-            Console.WriteLine("   |   |   ");
-            Console.WriteLine(" {0} | {1} | {2} ",matrix[0,0], matrix[0,1], matrix[0,2]);
-            Console.WriteLine("___|___|___");
-            Console.WriteLine("   |   |   ");
-            Console.WriteLine(" {0} | {1} | {2} ",matrix[1,0], matrix[1,1], matrix[1,2]);
-            Console.WriteLine("___|___|___");
-            Console.WriteLine("   |   |   ");
-            Console.WriteLine(" {0} | {1} | {2} ",matrix[2,0], matrix[2,1], matrix[2,2]);
-            Console.WriteLine("   |   |   ");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ",matrix[0,0], matrix[0,1], matrix[0,2]);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ",matrix[1,0], matrix[1,1], matrix[1,2]);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ",matrix[2,0], matrix[2,1], matrix[2,2]);
+            Console.WriteLine("     |     |     ");
         }
         public static void PlayerOne()
         {
