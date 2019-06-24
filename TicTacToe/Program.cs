cd06ba733e8d3d7d3d0d7ad81298d4d929740a35
Jesse Barajas <jb19932010@yahoo.com>
2019-06-24 13:07:25 -0500

    Removed Duplication
    took out duplicate code for different players. Made 1 method that changes dependant on the argument entered.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -11,7 +11,8 @@ namespace TicTacToe
     {
         static bool gameongoing = true;
         static bool GameOver = false;
-        static string winningPlayer = "0";
+        static int winningPlayer = 0;
+        static string playerSign;
         static int[] usedFields = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         static string[,] matrix =
         { 
@@ -27,14 +28,14 @@ namespace TicTacToe
                 CreateTicTacToe();
                 while (GameOver == false)
                 {
-                    PlayerOne();
-                    CheckWinOne();
+                    PlayerMove(1);
+                    CheckWin(1);
                     if (GameOver == true)
                     {
                         break;
                     }
-                    PlayerTwo();
-                    CheckWinTwo();
+                    PlayerMove(2);
+                    CheckWin(2);
                     if (GameOver == true)
                     {
                         break;
@@ -58,14 +59,21 @@ namespace TicTacToe
             Console.WriteLine("  {0}  |  {1}  |  {2}  ",matrix[2,0], matrix[2,1], matrix[2,2]);
             Console.WriteLine("     |     |     ");
         }
-        public static void PlayerOne()
+        public static void PlayerMove(int player)
         {
             bool noRightInput = true;
+            if (player == 1)
+            {
+                playerSign = "X";
+            }else if (player == 2)
+            {
+                playerSign = "O";
+            }
             while (noRightInput)
             {
                 bool numberUsed = false;
                 int inputNumber;
-                Console.Write("Player 1: Choose your field:");
+                Console.Write("Player {0}: Choose your field for {1}:", player, playerSign);
                 string input = Console.ReadLine();
                 bool success = int.TryParse(input, out inputNumber);
                 if (success)
@@ -78,6 +86,7 @@ namespace TicTacToe
                             {
                                 numberUsed = true;
                                 Console.WriteLine("Number has been selected before.");
+                                break;
                             }
                         }
                         if (numberUsed == false)
@@ -85,39 +94,39 @@ namespace TicTacToe
                             switch (inputNumber)
                             {
                                 case 1:
-                                    matrix[0, 0] = "X";
+                                    matrix[0, 0] = playerSign;
                                     usedFields[0] = 1;
                                     break;
                                 case 2:
-                                    matrix[0, 1] = "X";
+                                    matrix[0, 1] = playerSign;
                                     usedFields[1] = 2;
                                     break;
                                 case 3:
-                                    matrix[0, 2] = "X";
+                                    matrix[0, 2] = playerSign;
                                     usedFields[2] = 3;
                                     break;
                                 case 4:
-                                    matrix[1, 0] = "X";
+                                    matrix[1, 0] = playerSign;
                                     usedFields[3] = 4;
                                     break;
                                 case 5:
-                                    matrix[1, 1] = "X";
+                                    matrix[1, 1] = playerSign;
                                     usedFields[4] = 5;
                                     break;
                                 case 6:
-                                    matrix[1, 2] = "X";
+                                    matrix[1, 2] = playerSign;
                                     usedFields[5] = 6;
                                     break;
                                 case 7:
-                                    matrix[2, 0] = "X";
+                                    matrix[2, 0] = playerSign;
                                     usedFields[6] = 7;
                                     break;
                                 case 8:
-                                    matrix[2, 1] = "X";
+                                    matrix[2, 1] = playerSign;
                                     usedFields[7] = 8;
                                     break;
                                 case 9:
-                                    matrix[2, 2] = "X";
+                                    matrix[2, 2] = playerSign;
                                     usedFields[8] = 9;
                                     break;
                             }
@@ -135,122 +144,22 @@ namespace TicTacToe
                 }
             }
         }
-        public static void PlayerTwo()
-        {
-            bool noRightInput = true;
-            while (noRightInput)
-            {
-                bool numberUsed = false;
-                int inputNumber;
-                Console.Write("Player 2: Choose your field:");
-                string input = Console.ReadLine();
-                bool success = int.TryParse(input, out inputNumber);
-                if (success)
-                {
-                    if (inputNumber > 0 & inputNumber < 10)
-                    {
-                        foreach (int k in usedFields)
-                        {
-                            if (inputNumber == k)
-                            {
-                                numberUsed = true;
-                                Console.WriteLine("Number has been selected before.");
-                            }
-                        }
-                        if (numberUsed == false)
-                        {
-                            switch (inputNumber)
-                            {
-                                case 1:
-                                    matrix[0, 0] = "O";
-                                    usedFields[0] = 1;
-                                    break;
-                                case 2:
-                                    matrix[0, 1] = "O";
-                                    usedFields[1] = 2;
-                                    break;
-                                case 3:
-                                    matrix[0, 2] = "O";
-                                    usedFields[2] = 3;
-                                    break;
-                                case 4:
-                                    matrix[1, 0] = "O";
-                                    usedFields[3] = 4;
-                                    break;
-                                case 5:
-                                    matrix[1, 1] = "O";
-                                    usedFields[4] = 5;
-                                    break;
-                                case 6:
-                                    matrix[1, 2] = "O";
-                                    usedFields[5] = 6;
-                                    break;
-                                case 7:
-                                    matrix[2, 0] = "O";
-                                    usedFields[6] = 7;
-                                    break;
-                                case 8:
-                                    matrix[2, 1] = "O";
-                                    usedFields[7] = 8;
-                                    break;
-                                case 9:
-                                    matrix[2, 2] = "O";
-                                    usedFields[8] = 9;
-                                    break;
-                            }
-                            noRightInput = false;
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Number is out of bounds. Please enter a legitimate field.");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Wrong format entered. Please enter a legitimate field.");
-                }
-            }
-        }
-        public static void CheckWinOne()
+
+        public static void CheckWin(int player)
         {
             Console.Clear();
             CreateTicTacToe();
-            if ((matrix[0,0] == "X" & matrix[0,1] == "X" & matrix[0,2] == "X") ||
-                (matrix[1,0] == "X" & matrix[1,1] == "X" & matrix[1,2] == "X") ||
-                (matrix[2,0] == "X" & matrix[2,1] == "X" & matrix[2,2] == "X") ||
-                (matrix[0,0] == "X" & matrix[1,0] == "X" & matrix[2,0] == "X") ||
-                (matrix[0,1] == "X" & matrix[1,1] == "X" & matrix[2,1] == "X") ||
-                (matrix[0,2] == "X" & matrix[1,2] == "X" & matrix[2,2] == "X") ||
-                (matrix[0,0] == "X" & matrix[1,1] == "X" & matrix[2,2] == "X") ||
-                (matrix[0,2] == "X" & matrix[1,1] == "X" & matrix[2,0] == "X"))
+            if ((matrix[0,0] == playerSign & matrix[0,1] == playerSign & matrix[0,2] == playerSign) ||
+                (matrix[1,0] == playerSign & matrix[1,1] == playerSign & matrix[1,2] == playerSign) ||
+                (matrix[2,0] == playerSign & matrix[2,1] == playerSign & matrix[2,2] == playerSign) ||
+                (matrix[0,0] == playerSign & matrix[1,0] == playerSign & matrix[2,0] == playerSign) ||
+                (matrix[0,1] == playerSign & matrix[1,1] == playerSign & matrix[2,1] == playerSign) ||
+                (matrix[0,2] == playerSign & matrix[1,2] == playerSign & matrix[2,2] == playerSign) ||
+                (matrix[0,0] == playerSign & matrix[1,1] == playerSign & matrix[2,2] == playerSign) ||
+                (matrix[0,2] == playerSign & matrix[1,1] == playerSign & matrix[2,0] == playerSign))
             {
                 GameOver = true;
-                winningPlayer = "1";
-            }
-            if (
-            matrix[0, 0] != "1" & matrix[0, 1] != "2" & matrix[0, 2] != "3" &
-            matrix[1, 0] != "4" & matrix[1, 1] != "5" & matrix[1, 2] != "6" &
-            matrix[2, 0] != "7" & matrix[2, 1] != "8" & matrix[2, 2] != "9")
-            {
-                GameOver = true;
-            }
-        }
-        public static void CheckWinTwo()
-        {
-            Console.Clear();
-            CreateTicTacToe();
-            if ((matrix[0, 0] == "O" & matrix[0, 1] == "O" & matrix[0, 2] == "O") ||
-                (matrix[1, 0] == "O" & matrix[1, 1] == "O" & matrix[1, 2] == "O") ||
-                (matrix[2, 0] == "O" & matrix[2, 1] == "O" & matrix[2, 2] == "O") ||
-                (matrix[0, 0] == "O" & matrix[1, 0] == "O" & matrix[2, 0] == "O") ||
-                (matrix[0, 1] == "O" & matrix[1, 1] == "O" & matrix[2, 1] == "O") ||
-                (matrix[0, 2] == "O" & matrix[1, 2] == "O" & matrix[2, 2] == "O") ||
-                (matrix[0, 0] == "O" & matrix[1, 1] == "O" & matrix[2, 2] == "O") ||
-                (matrix[0, 2] == "O" & matrix[1, 1] == "O" & matrix[2, 0] == "O"))
-            {
-                GameOver = true;
-                winningPlayer = "2";
+                winningPlayer = player;
             }
             if (
             matrix[0, 0] != "1" & matrix[0, 1] != "2" & matrix[0, 2] != "3" &
@@ -262,7 +171,7 @@ namespace TicTacToe
         }
         public static void WinnerAnnouncement()
         {
-            if (winningPlayer != "0")
+            if (winningPlayer != 0)
             {
             Console.WriteLine("Game Over! Player {0} is the winner!", winningPlayer);
             }
@@ -293,7 +202,7 @@ namespace TicTacToe
                 matrix[2, 1] = "8";
                 matrix[2, 2] = "9";
                 Console.Clear();
-                winningPlayer = "0";
+                winningPlayer = 0;
                 GameOver = false;
             }
             else
